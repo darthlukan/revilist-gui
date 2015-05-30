2a388ce9b1bbbd097e03fd5c46f2e9f2073302a7
Brian Tomlinson <darthlukan@gmail.com>
2015-05-30 21:36:33 +0200

    add a blank login controller

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,11 @@
+/**
+ * @ngdoc function
+ * @name revilistApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the revilistApp
+ */
+ angular.module('revilistApp')
+   .controller('MainCtrl', function ($scope) {
+     this.user = {};
+   });
