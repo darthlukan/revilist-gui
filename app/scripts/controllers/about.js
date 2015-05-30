c91ed38cb7f7eb168ec7cbd4eaab4af6eb2e9b6a
Brian Tomlinson <darthlukan@gmail.com>
2015-05-30 13:22:59 +0200

    initial commit, yeoman angular generator default no sass

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name revilistApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the revilistApp
+ */
+angular.module('revilistApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
