47716c2f7265937b8906ed984e07fabb9b8da469
Brian Tomlinson <darthlukan@gmail.com>
2015-05-30 19:32:14 +0200

    allow editing of docs inline

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,21 +13,33 @@ angular.module('revilistApp')
       {
         title: 'A title',
         revision: 1,
-        status: '02'
+        status: '02',
+        edit: false
       },
       {
         title: 'Another title',
         revision: 3,
-        status: '01'
+        status: '01',
+        edit: false
+      },
+      {
+        title: 'Yet another title',
+        revision: 1,
+        status: '05',
+        edit: false
       }
     ];
+
     this.awesomeThings = {
       'html': 'HTML5 Boilerplate',
       'angular': 'AngularJS',
       'karma': 'Karma'
     };
+
     this.selected = 1;
+
     this.isSelected = function(value) {
       return this.selected === value;
     };
+
   });
