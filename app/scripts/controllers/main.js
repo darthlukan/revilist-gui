660f4212f3ae1ed9a3ffbc7ee0d9fdd6111d3f00
Brian Tomlinson <darthlukan@gmail.com>
2015-05-30 21:26:57 +0200

    toying with dates and row coloring

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,12 +12,14 @@ angular.module('revilistApp')
     this.documents = [];
     this.projects = [];
     this.populate = function() {
-      for (var i = 1; i <= 25; i++) {
+      for (var i = 1; i <= 10; i++) {
         this.documents.push({
           title: 'Title ' + i,
           revision: i,
           status: i < 10 ? '0' + i: i,
-          edit: false
+          edit: false,
+          dangerous: i % 3 === 0 || i % 5 === 0 ? true : false,
+          duedate: Date.now()
         });
         if (i < 7) {
           this.projects.push({
