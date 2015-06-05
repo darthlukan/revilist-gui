935b50b9abb6aabe7dc1184cae7b77893735b146
Brian Tomlinson <darthlukan@gmail.com>
2015-06-05 21:41:46 +0200

    more stuff

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -9,4 +9,12 @@
 */
 angular.module('revilistApp')
   .controller('UserCtrl', function () {
+    this.user = {
+      username: 'Foo',
+      email: 'bar@example.com',
+      authed: true,
+      company: 'ACME Corp.',
+      projects: [1, 2, 3],
+      profileImg: '/images/yeoman.png'
+    };
   });
